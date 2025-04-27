9d72efc436938032c5060a6ac3743a02a16eb41d
najasinis <tttjjjyyy1234@daum.net>
2025-04-28 05:28:12 +0900

    update file

--- src/App.js
+++ src/App.js
@@ -2,22 +2,50 @@ import React, {Component} from 'react';
 import './App.css';
 import Customer from './components/customer';
 
-const customers = {
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
     'name': '나자신',
     'birthday': '961222',
     'gender': '남자',
     'job': '대학생'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '정진영',
+    'birthday': '961022',
+    'gender': '남자',
+    'job': '대학생'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '밉지않은관종',
+    'birthday': '961122',
+    'gender': '남자',
+    'job': '대학생'
   }
-
+]
 class App extends Component {
   render() {
     return (
-      <Customer 
-        name={customers.name}
-        birthday={customers.birthday}
-        gender={customers.gender}
-        job={customers.job}
-      />
+      <div>
+        {customers.map(c => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </div>
     );
   }
 }
